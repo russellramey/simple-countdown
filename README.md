29ed3524f77e468b5134c745f5b1e0e8dffcd79e
Russell <doublercreative@gmail.com>
2016-06-16 16:31:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Synopsis
+
+Simple countdown timer, mainly used for an event or countdown to day done in plain javascript with no dependencies.
---
+++ simple.countdown.js
@@ -0,0 +1,47 @@
+// Simple Countdown
+function simple_countdown() {
+	// Set target date
+	var deadline = new Date("January 1, 2017");
+	var now = new Date();
+
+	// Set element id's
+	var elmDays = "daysTxt",
+	    elmHours = "hoursTxt",
+	    elmMins = "minsTxt",
+	    elmSecs = "secsTxt";
+
+	// Set time difference
+	var timeDiff = deadline.getTime() - now.getTime();
+
+	if (timeDiff <= 0) {
+
+		document.getElementById(elmDays).innerHTML = 0;
+		document.getElementById(elmHours).innerHTML = 0;
+		document.getElementById(elmMins).innerHTML = 0;
+		document.getElementById(elmSecs).innerHTML = 0;
+
+    } else {
+
+    	// Calculate time digits
+		var seconds = Math.floor(timeDiff / 1000),
+			minutes = Math.floor(seconds / 60),
+			hours = Math.floor(minutes / 60),
+			days = Math.floor(hours / 24);
+
+			hours %= 24;
+	        minutes %= 60;
+	        seconds %= 60;
+
+	    // Print html
+		document.getElementById(elmDays).innerHTML = days;
+		document.getElementById(elmHours).innerHTML = hours;
+		document.getElementById(elmMins).innerHTML = minutes;
+		document.getElementById(elmSecs).innerHTML = seconds;
+
+		// Set timer speed
+		var timer = setTimeout('simple_countdown()',1000);
+	}
+}
+
+// Run function
+simple_countdown();
